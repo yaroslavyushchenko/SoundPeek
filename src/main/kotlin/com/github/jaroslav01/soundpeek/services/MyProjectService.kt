f50e5ef346d7f8c238d1188d920cf707945ed1c9
GitHub Action <action@github.com>
2025-06-06 14:01:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/jaroslav01/soundpeek/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jaroslav01/soundpeek/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jaroslav01.soundpeek.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jaroslav01.soundpeek.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
